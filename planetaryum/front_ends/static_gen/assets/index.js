4dc5603e4239c79c1ade4412cd45d2febc8b846c
Luca De Feo <defeo@pippin>
2018-08-18 02:05:53 +0200

    Improved VueJS front end

--- planetaryum/front_ends/static_gen/assets/index.js
+++ planetaryum/front_ends/static_gen/assets/index.js
@@ -2,25 +2,69 @@ async function fetch_meta() {
     return await (await fetch('meta.json')).json();
 }
 
-function init_components() {
-    Vue.component('thumb', {
-	props: ['nb'],
-	template: '<div>{{ nb.name }}</div>',
-    });
-}
-
-
 (async function() {
-    try {
-	let db = await fetch_meta();
+    let db_promise = fetch_meta();
+    
+    Vue.component('plyum-thumb', {
+	props: ['nb'],
+	template: `<div class="thumb">
+  <router-link v-bind:to="'/nb/' + nb._id">
+    <img v-bind:src="getThumb()">
+    <span>{{ nb.name }}</span>
+  </router-link>
+</div>`,
+	methods: {
+	    getThumb() {
+		// TODO: improve this
+		if (this.$props.nb.meta.thumbs.length) {
+		    return this.$props.nb.meta.thumbs[0];
+		} else {
+		    return '';
+		}
+	    },
+	},
+    });
 
-	init_components();
-	
+    const Notebook = {
+	data() {
+	    return {
+		loading: false,
+		html: null,
+	    };
+	},
+	created() {
+	    this.fetchNB();
+	},
+	watch: {
+	    '$route': 'fetchNB'
+	},
+	props: ['nb'],
+	template: `<div v-html="html"></div>`,
+	methods: {
+	    fetchNB() {
+	    	fetch(this.$props.nb.path)
+		    .then(res => res.text())
+		    .then(html => this.html = html);
+	    },
+	},
+    };
+
+    try {
+	const data = await db_promise;
 	let app = new Vue({
 	    el: '#planetaryum',
 	    data: {
-		db: db,
-	    }
+		db: data,
+	    },
+	    router: new VueRouter({
+		routes: data.map((nb) => ({
+		    path: `/nb/${nb._id}`,
+		    component: Notebook,
+		    props: { nb },
+		})),
+	    }),
+	    methods: {
+	    },
 	})
     } catch (e) {
 	console.error(e);
